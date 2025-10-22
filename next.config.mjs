568a737bc34eca25afc0da5c2958e9571a6234f0
Paulina <paulinagonzalezcantu@Paulinas-MacBook-Air.local>
2025-10-22 15:18:47 -0700

    Fix next.config.mjs for deployment

--- next.config.mjs
+++ next.config.mjs
@@ -2,6 +2,9 @@
 const nextConfig = {
   reactStrictMode: true,
   output: 'export', // 👈 This enables static export
+  images: {
+    unoptimized: true, // ✅ disables server-based image optimization
+  },
 };
 
 export default nextConfig;
